09f7246b490e5736ad25e0982c5be6fc5329ba1b
Shreyash Patel <shreyashpatel5506@gmail.com>
2026-07-15 23:00:31 +0530

    banner ads

--- app/layout.js
+++ app/layout.js
@@ -43,16 +43,16 @@ export default function RootLayout({ children }) {
 <meta name="referrer" content="no-referrer-when-downgrade" />
   <Script id="hilltopads" strategy="afterInteractive">
     {`
-      (function(owmnl){
-        var d = document,
-            s = d.createElement('script'),
-            l = d.scripts[d.scripts.length - 1];
-        s.settings = owmnl || {};
-        s.src = "//nautical-hand.com/c_Dg9.6lbs2_5GlOSgWgQt9QNszSI/ysNkTzk-yPOESp0w3nMojQI/1AOyT/M/zJ";
-        s.async = true;
-        s.referrerPolicy = "no-referrer-when-downgrade";
-        l.parentNode.insertBefore(s, l);
-      })({});
+    (function(jbm){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = jbm || {};
+s.src = "\/\/quarrelsomebitter.com\/byX\/VMsYd.GHli0nYnWjcH\/uefmw9\/ufZ-U\/lrkRPGTjcDy\/MjjGY\/wcNbTYc\/tPN\/zBI-yZNVjzA-2\/MIQe";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})
     `}
   </Script>
 </head>

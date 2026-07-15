5031bccfaa56524dbdc5e77db05ae6fe74f58787
Shreyash Patel <shreyashpatel5506@gmail.com>
2026-07-15 23:07:38 +0530

    ipagead

--- app/layout.js
+++ app/layout.js
@@ -55,6 +55,17 @@ l.parentNode.insertBefore(s, l);
 })({})
     `}
   </Script>
+  <Script id="hilltopads2" strategy="afterInteractive">{`(function(zchkt){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = zchkt || {};
+s.src = "\/\/quarrelsomebitter.com\/b.X\/VGsTdiGLlR0HYEW-cT\/UeTm_9vuPZVUMlpkjPRTOcoyjM\/jiYjwHOUD\/E\/t\/NlzXI\/yzN\/jfAO4zNRQS";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})`}
+</Script>
 </head>
       <body className="bg-bg-void text-text-primary min-h-full flex flex-col antialiased">
         {children}
